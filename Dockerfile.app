6af0d48bc8d5d9c467202cd90fbb5f28f5e6fad3
kobayashi/personal <ikuyorih.kobayashi@gmail.com>
2023-04-19 03:25:42 +0900

    feat: call get method from react-app

---
+++ Dockerfile.app
@@ -0,0 +1,10 @@
+#fastapi + uvicornでのアプリの実行
+FROM python:3.11-slim-buster
+
+WORKDIR /app
+RUN pip install poetry
+RUN poetry config virtualenvs.in-project true
+COPY poetry.lock* pyproject.toml* ./
+RUN poetry install --no-root --no-dev
+EXPOSE 8000
+CMD ["poetry", "run", "uvicorn", "mpt-app.main:app", "--host", "0.0.0.0", "--reload"]
---
+++ mpt-ui/src/App.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+import axios, {AxiosResponse} from "axios";
+
+type Hello = {
+  Hello: string;
+}
+function App() {
+	const [data, setData] = useState<Hello>();
+	const url: string = "http://127.0.0.1:8000";
+
+	const GetData = () => {
+		axios.get(url).then((res: AxiosResponse<Hello>) => {
+			setData(res.data);
+		});
+	};
+	return (
+		<div>
+			<div>ここに処理を書いていきます</div>
+			{data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
+		</div>
+	);
+}
+
+export default App;
