f9f8c537f01bec6a3548abbd097cc2d8fa5da8e2
kobayashi/personal <ikuyorih.kobayashi@gmail.com>
2023-06-30 21:58:59 +0900

    Chapter-Box, Chapter-Textの実装 Fixes #4

--- mpt_ui/src/views/App.tsx
+++ mpt_ui/src/views/App.tsx
@@ -41,13 +41,36 @@ const ContentsContainer: React.FC<ContentsContainerProps> = (props) => {
 	)
 }
 
+type ContentsFrameProps = {
+	children: React.ReactNode;
+}
+
+// コンテンツの背景
+const ContentsFrame: React.FC<ContentsFrameProps> = (props) => {
+	return (
+		<div className="App-Contents-Frame">
+			{props.children}
+		</div>
+	)
+}
+
 function App() {
+	const chapters: string[] = ['Chapter1', 'Chapter2', 'Chapter3', 'Chapter4', 'Chapter5', 'Chapter6']
 	return (
 		<div>
 			<AppHeaderContainer>
 				<Title title="Math Problems Tutor"/>
 			</AppHeaderContainer>
 			<ContentsContainer>
+				<ContentsFrame>
+					{chapters.map((chapter) => (
+						<div className="App-Chapter-Box">
+							<div className="App-Chapter-Text-Frame">
+								<p className="App-Chapter-Text">{chapter}</p>
+							</div>
+						</div>
+					))}
+				</ContentsFrame>
 			</ContentsContainer>
 		</div>
 	)
