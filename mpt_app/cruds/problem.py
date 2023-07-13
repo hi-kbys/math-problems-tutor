8f324bbb2b8cc3f155a4e4094d37a4e441d37dfa
kobayashi/personal <ikuyorih.kobayashi@gmail.com>
2023-07-13 22:03:02 +0900

    feat: problemのput/delete 実装 Fixes #12

--- mpt_app/cruds/problem.py
+++ mpt_app/cruds/problem.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from sqlalchemy import select
@@ -7,8 +7,12 @@ from sqlalchemy.engine import Result
 import mpt_app.models.problem as problem_model
 import mpt_app.schemas.problem as problem_schema
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 async def get_problems(
-        db: AsyncSession) -> Union[list[int, str, str, bool], None]:
+        db: AsyncSession) -> list[int, str, str, bool] | None:
     result: Result = await db.execute(select(
         problem_model.Problem.id, 
         problem_model.Problem.title, 
@@ -18,10 +22,37 @@ async def get_problems(
     )
     return result.all()
 
-async def create_problems(
-        db: AsyncSession, problem_create: problem_schema.ProblemCreate) -> problem_model.Problem:
+async def get_problem(db: AsyncSession, problem_id: int) -> problem_model.Problem | None:
+    result : Result  = await db.execute(
+        select(problem_model.Problem).filter(problem_model.Problem.id == problem_id)
+    )
+    problem : tuple(problem_model.Problem) | None = result.first()
+    return problem[0] if problem[0] is not None else None
+
+async def create_problem(
+        db: AsyncSession, problem_create: problem_schema.ProblemCreate
+        ) -> problem_model.Problem:
     problem = problem_model.Problem(**problem_create.dict())
     db.add(problem)
     await db.commit()
     await db.refresh(problem)
     return problem
+
+async def update_problem(
+        db: AsyncSession, 
+        problem_update: problem_schema.ProblemUpdate,
+        problem: problem_model.Problem
+        ) -> problem_model.Problem:
+    problem.title = problem_update.title
+    problem.statement = problem_update.statement
+    problem.is_solved = problem_update.is_solved
+    db.add(problem)
+    await db.commit()
+    await db.refresh(problem)
+    return problem
+
+async def delete_problem(
+        db: AsyncSession, problem: problem_model.Problem
+        ) -> None:
+    await db.delete(problem)
+    await db.commit()
--- tests/test_problems.py
+++ tests/test_problems.py
@@ -35,15 +35,72 @@ async def async_client():
     
 @pytest.mark.asyncio
 async def test_create_and_read(async_client: AsyncClient):
+    # post
     response = await async_client.post(
         "/problems",
         json={"title": "test", "statement": "test_statement"},
     )
     assert response.status_code == starlette.status.HTTP_200_OK
     
+    # read_all
     response = await async_client.get("/problems")
     assert response.status_code == starlette.status.HTTP_200_OK
     response_json = response.json()
     assert response_json[0]["title"] == "test"
     assert response_json[0]["statement"] == "test_statement"
+
+@pytest.mark.asyncio
+async def test_create_and_read_with_id(async_client: AsyncClient):
+    # post
+    response = await async_client.post(
+        "/problems",
+        json={"title": "test", "statement": "test_statement"},
+    )
+    assert response.status_code == starlette.status.HTTP_200_OK
     
+    # read problem with_id 1
+    response = await async_client.get("/problems/1")
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_json = response.json()
+    assert response_json["title"] == "test"
+    assert response_json["statement"] == "test_statement"
+
+@pytest.mark.asyncio
+async def test_update(async_client: AsyncClient):
+    # post
+    response = await async_client.post(
+        "/problems",
+        json={"title": "test", "statement": "test_statement"},
+    )
+    assert response.status_code == starlette.status.HTTP_200_OK
+    
+    # update
+    response = await async_client.get("/problems")
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_json = response.json()
+    assert response_json[0]["title"] == "test"
+    assert response_json[0]["statement"] == "test_statement"
+    assert response_json[0]["is_solved"] == False
+    
+    response = await async_client.put(
+        "/problems/1",
+        json={"title": "test_1", "statement": "test_statement_1", "is_solved": True},
+    )
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_json = response.json()
+    assert response_json["title"] == "test_1"
+    assert response_json["statement"] == "test_statement_1"
+
+@pytest.mark.asyncio
+async def test_delete(async_client: AsyncClient):
+    # post
+    response = await async_client.post(
+        "/problems",
+        json={"title": "test", "statement": "test_statement"},
+    )
+    assert response.status_code == starlette.status.HTTP_200_OK
+    
+    # delete
+    response = await async_client.delete("/problems/1")
+    assert response.status_code == starlette.status.HTTP_200_OK
+    assert response.json() is None
